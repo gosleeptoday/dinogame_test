795aebfa2fb34abc8fa476aa693e73575e2758fc
Fedor Tatarnikov <62100314+gosleeptoday@users.noreply.github.com>
2023-03-30 10:00:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace DinoGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.scoreText = new System.Windows.Forms.Label();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.trex = new System.Windows.Forms.PictureBox();
+            this.obstacle1 = new System.Windows.Forms.PictureBox();
+            this.obstacle2 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scoreText
+            // 
+            this.scoreText.AutoSize = true;
+            this.scoreText.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreText.Location = new System.Drawing.Point(16, 11);
+            this.scoreText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.scoreText.Name = "scoreText";
+            this.scoreText.Size = new System.Drawing.Size(93, 32);
+            this.scoreText.TabIndex = 0;
+            this.scoreText.Text = "Score: ";
+            // 
+            // floor
+            // 
+            this.floor.BackColor = System.Drawing.Color.SaddleBrown;
+            this.floor.Location = new System.Drawing.Point(-11, 423);
+            this.floor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(800, 31);
+            this.floor.TabIndex = 1;
+            this.floor.TabStop = false;
+            // 
+            // trex
+            // 
+            this.trex.Image = global::DinoGame.Properties.Resources.running;
+            this.trex.Location = new System.Drawing.Point(132, 348);
+            this.trex.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(59, 74);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.trex.TabIndex = 2;
+            this.trex.TabStop = false;
+            // 
+            // obstacle1
+            // 
+            this.obstacle1.Image = global::DinoGame.Properties.Resources.obstacle_1;
+            this.obstacle1.Location = new System.Drawing.Point(563, 366);
+            this.obstacle1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.obstacle1.Name = "obstacle1";
+            this.obstacle1.Size = new System.Drawing.Size(23, 46);
+            this.obstacle1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.obstacle1.TabIndex = 3;
+            this.obstacle1.TabStop = false;
+            this.obstacle1.Tag = "obstacle";
+            // 
+            // obstacle2
+            // 
+            this.obstacle2.Image = global::DinoGame.Properties.Resources.obstacle_2;
+            this.obstacle2.Location = new System.Drawing.Point(704, 366);
+            this.obstacle2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.obstacle2.Name = "obstacle2";
+            this.obstacle2.Size = new System.Drawing.Size(61, 57);
+            this.obstacle2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.obstacle2.TabIndex = 4;
+            this.obstacle2.TabStop = false;
+            this.obstacle2.Tag = "obstacle";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 444);
+            this.Controls.Add(this.obstacle2);
+            this.Controls.Add(this.obstacle1);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.floor);
+            this.Controls.Add(this.scoreText);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obstacle2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label scoreText;
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox obstacle1;
+        private System.Windows.Forms.PictureBox obstacle2;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinoGame
+{
+    public partial class Form1 : Form
+    {
+        bool jumping = false; 
+        int jumpSpeed = 20;
+        int force = 5;
+        int obstacleSpeed = 3;
+        int score = 0;
+        Random random = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && !jumping)
+            {
+                jumping = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                resetGame();
+            }
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+
+        private void resetGame()
+        {
+            jumping = false;
+            jumpSpeed = 15;
+            force = 3;
+            obstacleSpeed = 1;
+            score = 0;
+            trex.Top = floor.Top - trex.Height;
+            trex.Image = Properties.Resources.running;
+
+            foreach (Control x in this.Controls)
+            {
+                int position = random.Next(600, 1000);
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left = 600 + (x.Left + position + x.Width * 3);        
+                }
+            }
+            gameTimer.Start();
+        }
+
+        private void gameEvent()
+        {
+            trex.Top += jumpSpeed;
+
+            scoreText.Text = "Score: " + score;
+
+
+            if(jumping && force < 0)
+            {
+                jumping = false;
+            }
+            if(jumping)
+            {
+                jumpSpeed = -15;
+                force -= 1;
+            }
+            else {
+                jumpSpeed = 15;
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+                    if (x.Left + x.Width < -120)
+                    {
+                        x.Left = this.ClientSize.Width + random.Next(200, 800);
+                        score++;
+                    }
+                    if (trex.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        gameTimer.Stop();
+                        trex.Image = Properties.Resources.dead;
+                        scoreText.Text += " Press R to restart the game!";
+                    }
+                }
+            }
+
+            if((trex.Top + trex.Height > floor.Top ) && (!jumping))
+            {
+                force = 5;
+                trex.Top = floor.Top - trex.Height;
+                jumpSpeed = 0;
+            }
+
+            if(score%20 == 0)
+            {
+                obstacleSpeed += 3;
+            }
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            gameEvent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
